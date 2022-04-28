03cf288ca0648e841188f8c73988c59e18c90e86
Chris Perry <chris.perry@ninjarmm.com>
2022-04-28 08:22:49 -0500

    Switched weather api from openweathermap to apininjas

--- weather/src/main/java/com/ninjaone/weatherservice/domain/WeatherData.java
+++ weather/src/main/java/com/ninjaone/weatherservice/domain/WeatherData.java
@@ -1,48 +1,67 @@
 package com.ninjaone.weatherservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Value
 @Builder
 @Jacksonized
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherData {
 
-    @JsonProperty("coord")
-    Coordinates coordinates;
+    @JsonProperty("cloud_pct")
+    Integer cloudPercentage;
 
-    @JsonProperty("weather")
-    List<WeatherSummary> summaries;
+    @JsonProperty("temp")
+    Integer temperature;
 
-    String base;
+    @JsonProperty("feels_like")
+    Integer feelsLike;
 
-    @JsonProperty("main")
-    CurrentConditions currentConditions;
+    Integer humidity;
 
-    Integer visibility;
+    @JsonProperty("min_temp")
+    Integer lowTemperature;
 
-    Wind wind;
+    @JsonProperty("max_temp")
+    Integer highTemperature;
 
-    Clouds clouds;
+    @JsonProperty("wind_speed")
+    BigDecimal windSpeed;
 
-    Integer dt;
+    @JsonProperty("wind_degrees")
+    Integer windDegrees;
 
-    Integer timezone;
+    Integer sunrise;
 
-    Integer id;
+    Integer sunset;
 
-    String name;
-
-    Integer cod;
-
-    @JsonProperty("sys")
-    WeatherSystem weatherSystem;
+    @JsonGetter("wind_direction")
+    public String windDirection() {
+        if (windDegrees >= 23 && windDegrees < 68) {
+            return "North East";
+        } else if (windDegrees >= 68 && windDegrees < 113) {
+            return "East";
+        } else if (windDegrees >= 113 && windDegrees < 158) {
+            return "South East";
+        } else if (windDegrees >= 158 && windDegrees < 203) {
+            return "South";
+        } else if (windDegrees >= 203 && windDegrees < 248) {
+            return "South West";
+        } else if (windDegrees >= 248 && windDegrees < 293) {
+            return "West";
+        } else if (windDegrees >= 293 && windDegrees < 338) {
+            return "West";
+        } else {
+            return "North";
+        }
+    }
 
 }
 
---
+++ weather/src/test/java/com/ninjaone/weatherservice/services/WeatherServiceTest.java
@@ -0,0 +1,9 @@
+package com.ninjaone.weatherservice.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WeatherServiceTest {
+
+
+
+}
