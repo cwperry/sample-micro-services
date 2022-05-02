cc69a82c8cd9d35fb2a4249db32a7783ef3615ad
Chris Perry <chris.perry@ninjarmm.com>
2022-05-02 11:08:28 -0500

    Finished Fact-Service

--- fact-service/src/main/java/com/ninjaone/factservice/services/FactService.java
+++ fact-service/src/main/java/com/ninjaone/factservice/services/FactService.java
@@ -1,11 +1,43 @@
 package com.ninjaone.factservice.services;
 
 import com.ninjaone.factservice.domain.Fact;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestOperations;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class FactService {
+    private final RestOperations restOperations;
+    private final String url;
+    private final String apiKey;
+
+    public FactService(RestOperations restOperations,
+                       @Value("${weather.api.url}") String url,
+                       @Value("${weather.api.key}") String apiKey) {
+        this.restOperations = restOperations;
+        this.url = url;
+        this.apiKey = apiKey;
+    }
+
     public Fact retrieveFact() {
-        return null;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Api-Key", apiKey);
+
+        HttpEntity<List<Fact>> request = new HttpEntity<>(headers);
+
+        List<Fact> facts = restOperations.exchange(
+                url,
+                HttpMethod.GET,
+                request,
+                new ParameterizedTypeReference<List<Fact>>() {
+                }).getBody();
+        return Objects.requireNonNull(facts).stream().findFirst().orElse(null);
     }
 }
---
+++ fact-service/src/test/java/com/ninjaone/factservice/services/FactServiceTest.java
@@ -0,0 +1,64 @@
+package com.ninjaone.factservice.services;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.Resources;
+import com.ninjaone.factservice.domain.Fact;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestOperations;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FactServiceTest {
+
+    @Mock
+    private RestOperations restOperations;
+
+    private FactService factService;
+
+    @BeforeEach
+    public void beforeEach() {
+        factService = new FactService(restOperations, "http://localhost/fact", "api-key");
+    }
+
+    @Test
+    public void retrieveFact() throws IOException {
+        URL resource = Resources.getResource("samples/facts.json");
+        String json = Resources.toString(resource, Charset.defaultCharset());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Fact> facts = objectMapper.readValue(json, new TypeReference<>() {});
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Api-Key", "api-key");
+        HttpEntity<Fact> request = new HttpEntity<>(headers);
+
+        when(restOperations.exchange(
+                "http://localhost/fact",
+                HttpMethod.GET,
+                request,
+                new ParameterizedTypeReference<List<Fact>>(){}))
+                .thenReturn(ResponseEntity.ok(facts));
+
+        assertThat(factService.retrieveFact()).isEqualTo(facts.get(0));
+    }
+
+}
