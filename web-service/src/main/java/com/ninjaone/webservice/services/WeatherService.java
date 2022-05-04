8c9111b4dba2f0432516ce9676dfec7b916dde75
Chris Perry <chris.perry@ninjarmm.com>
2022-05-04 10:28:19 -0500

    Implemented WeatherService

--- web-service/src/main/java/com/ninjaone/webservice/services/WeatherService.java
+++ web-service/src/main/java/com/ninjaone/webservice/services/WeatherService.java
@@ -1,11 +1,28 @@
 package com.ninjaone.webservice.services;
 
 import com.ninjaone.webservice.domain.WeatherData;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestOperations;
 
 @Service
 public class WeatherService {
+    private final RestOperations restOperations;
+    private final String host;
+    private final int port;
+
+    public WeatherService(RestOperations restOperations,
+                          @Value("${weather.service.host}") String host,
+                          @Value("${weather.service.port}") int port) {
+        this.restOperations = restOperations;
+        this.host = host;
+        this.port = port;
+    }
+
     public WeatherData retrieveWeather(String zipCode) {
-        return null;
+        return restOperations.getForEntity(
+                String.format("http://%s:%d/weather?zip=%s", host, port, zipCode),
+                WeatherData.class)
+                .getBody();
     }
 }
